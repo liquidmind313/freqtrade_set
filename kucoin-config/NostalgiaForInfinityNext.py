2f9fa0a91a11e4a39ef11d6ce447331c86a25b22
Liquid Mind <73368693+liquidmind313@users.noreply.github.com>
2021-10-30 12:56:53 +0200

    Update NostalgiaForInfinityNext.py

--- kucoin-config/NostalgiaForInfinityNext.py
+++ kucoin-config/NostalgiaForInfinityNext.py
@@ -30,7 +30,7 @@ else:
 
 
 ###########################################################################################################
-##                NostalgiaForInfinityV8 by iterativ    V 7.7.2 USDT Kucoin                              ##
+##                NostalgiaForInfinityNext by iterativ    V 7.7.2 USDT Kucoin                                ##
 ##                                                                                                       ##
 ##    Strategy for Freqtrade https://github.com/freqtrade/freqtrade                                      ##
 ##                                                                                                       ##
@@ -79,7 +79,7 @@ else:
 ###########################################################################################################
 
 
-class NFiNext_772USDT_Kucoin(IStrategy):
+class NostalgiaForInfinityNext(IStrategy):
     INTERFACE_VERSION = 2
 
     plot_config = {
